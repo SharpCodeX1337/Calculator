7298088042d0fc890728015c50b36547da34ee6f
Shawon Chowdhury <124456552+SharpCodeX1337@users.noreply.github.com>
2023-02-05 01:59:37 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#learing 02/05/2023
+#sharpcodex
+
+ 
+print("Please input your digits")
+inpnum1 = input()
+print("Put any other random digits again")
+inpnum2 = input()
+print("The Answer is", int (inpnum1)+int (inpnum2) )
